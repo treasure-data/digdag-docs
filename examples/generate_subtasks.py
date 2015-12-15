ab8e9325ee87fbed7fd61778425e6875448a6b7b
Sadayuki Furuhashi <frsyuki@gmail.com>
2015-12-15 15:21:09 +0900

    added the first document

---
+++ examples/generate_subtasks.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+import digdag
+
+class ParallelProcess(digdag.BaseTask):
+    def split(self):
+        self.carry_params["task_count"] = 3
+
+    def run(self, task_count):
+        for i in range(task_count):
+            self.add_subtask(ParallelProcess.subtask, index=i)
+        self.subtask_config["parallel"] = True
+
+    def subtask(self, index):
+        print("Processing " + str(index))
+
